2aa690c4e267160447bee07531b208adcce5dc0b
KusumaBodapati <bodapatikusuma1311@gmail.com>
2025-10-18 10:35:20 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+
 import Taskform from "./Components/Taskform";
 import TaskList from "./Components/TaskList";
 import Progresstracker from "./Components/Progresstracker";
 import { useState } from "react";
 import { useEffect} from "react"
-
+import "./Style.css";
 function App() {
   const [tasks, setTasks] = useState([]);
 
@@ -14,14 +15,35 @@ function App() {
   const addTask = (task) => {
     setTasks([...tasks,task]);
   }
+
+  const updateTask = (updatedTask, index) => {
+    const newTask = [...tasks];
+    newTask[index] = updatedTask;
+    setTasks(newTask);
+  }
+
+  const deleteTask = (index) => {
+    setTasks(tasks.filter((_, i) => i != index));
+  }
+
+  const clearTasks = () => {
+    setTasks([]);
+  }
+
   return(
-    <div>
-      <h1>Task Focus</h1>
-      <p>My friendly TaskManager</p>
+    <div className="App">
+      <header>
+        <h1 className="title">Task Focus</h1>
+      <p className="tagline">My friendly TaskManager</p>
+      </header>
+      
       <Taskform addTask = {addTask}/>
-      <TaskList />
-      <Progresstracker />
-      <button>Clear all tasks</button>
+      <TaskList tasks = {tasks} 
+      updateTask = {updateTask} deleteTask = {deleteTask}/>
+      <Progresstracker tasks = {tasks}/>
+
+      {tasks.length>0 &&
+      (<button onClick={clearTasks} className="clear-btn">Clear all tasks</button>)}
     </div>
   )
 }
